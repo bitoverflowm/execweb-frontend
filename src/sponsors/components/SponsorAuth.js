3a1cd81ee2125d06dacac963eb1d01ba178e19b9
RikeshThapa <rthapa@u.rochester.edu>
2021-01-27 14:23:44 -0600

    adding sponsorAuth

--- src/sponsors/components/SponsorAuth.js
+++ src/sponsors/components/SponsorAuth.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
 import { Button, message } from 'antd';
+import { DatePicker, Space } from 'antd';
 
 class SponsorAuth extends React.Component{
 
     render(){
+        function onChange(date, dateString) {
+            console.log(date, dateString);
+          }
+
         return(
             <React.Fragment>
                 <h2>IntroEQ</h2>
                 <p>Let us know who your target clients are and sponsor only relevant to you round tables.</p>
                 <p>We need to verify yout professional identity.</p>
                 <Button type="primary" onClick={() => message.success('Registration complete!')}>Apply by LinkedIn</Button>
+                <DatePicker onChange={onChange} />
             </React.Fragment>
         );
     }   
