31469b136a85339b28e2caef4b75747d13ca5fd0
RikeshThapa <rthapa@u.rochester.edu>
2021-02-10 03:53:49 -0600

    finished state management for Sponsorship submission. Tried and tested

--- src/sponsors/components/SponsorTargetHost.js
+++ src/sponsors/components/SponsorTargetHost.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import '../../index.css';
 
@@ -6,18 +6,46 @@ import { Col, Checkbox, Typography } from 'antd';
 
 const { Text } = Typography;
 
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case 'CLICK':
+            return {
+                ...state,
+                value: action.val,
+                isValid: true
+            };    
+        default:
+            return state;
+    }
+
+};
 
 const SponsorTargetHost = props => {
 
-    const onChange = (checkedValues) => {
-        console.log('checked=', checkedValues);
-        props.handleHostSelection(checkedValues);
-    }
+    const [inputState, dispatch] = useReducer(inputReducer, {
+        value: '', 
+        isValid: false
+    });
+
+    const { id, formUpdateHandler } = props;
+    const { value, isValid } = inputState;
+
+    useEffect(() => {
+        formUpdateHandler(id, value, isValid)
+    }, [id, value, isValid, formUpdateHandler]);
+
+    const clickHandler = checkedValues => {
+        console.log('Selected value', checkedValues);
+        dispatch({
+            type: 'CLICK', 
+            val: checkedValues
+        });
+    };
 
     return(
         <div className = 'response-field'>
             <p>Do you want us to source a host or do yuo have one in mind?</p>
-            <Checkbox.Group onChange={onChange} style={{width : '100%'}}>
+            <Checkbox.Group onChange={clickHandler} style={{width : '100%'}}>
                 <Col span={12} className="check-box">
                     <Checkbox value={1}>                               
                         <Text strong>I have my own host. </Text>
@@ -28,7 +56,7 @@ const SponsorTargetHost = props => {
                         <Text strong>Please provide me with a host </Text> 
                     </Checkbox>
                 </Col>
-            </Checkbox.Group>
+            </Checkbox.Group> 
         </div>
     );   
 }
--- src/sponsors/components/SponsorTargetTopic.js
+++ src/sponsors/components/SponsorTargetTopic.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import '../../index.css';
 
@@ -6,21 +6,49 @@ import { Col, Checkbox, Typography } from 'antd';
 
 const { Text } = Typography;
 
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case 'CLICK':
+            return {
+                ...state,
+                value: action.val,
+                isValid: true
+            };    
+        default:
+            return state;
+    }
+
+};
 
 const SponsorTargetTopic = props => {
 
-    const onChange = (checkedValues) => {
-        console.log('checked=', checkedValues);
-        props.handleTopicSelection(checkedValues);
-    }
+    const [inputState, dispatch] = useReducer(inputReducer, {
+        value: '', 
+        isValid: false
+    });
+
+    const { id, formUpdateHandler } = props;
+    const { value, isValid } = inputState;
+
+    useEffect(() => {
+        formUpdateHandler(id, value, isValid)
+    }, [id, value, isValid, formUpdateHandler]);
+
+    const clickHandler = checkedValues => {
+        console.log('Selected value', checkedValues);
+        dispatch({
+            type: 'CLICK', 
+            val: checkedValues
+        });
+    };
 
     return(
         <div className = 'response-field'>
             <p>Do you have a topic in mind?</p>
-            <Checkbox.Group onChange={onChange} style={{width : '100%'}}>
+            <Checkbox.Group onChange={clickHandler} style={{width : '100%'}}>
                 <Col span={12} className="check-box">
                     <Checkbox value={1}>                               
-                        <Text strong>I have my own </Text>
+                        <Text strong>I have my own </Text> 
                     </Checkbox>
                 </Col>
                 <Col span={12} className="check-box">
