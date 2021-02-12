3164e6741aab03a5fe47f1036986405764520690
RikeshThapa <rthapa@u.rochester.edu>
2021-02-12 04:44:33 -0600

    adding npm serve and prod

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-
-import Home from './Home';
-import SponsorHome from './sponsors/pages/sponsorHome';
-import SponsorRequest from './sponsors/pages/SponsorRequest';
-import AdminDashboard from './admin/pages/AdminDashboard';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
-import SponsorshipRequestDetails from './admin/components/SponsorshipRequestDetails';
-
-import { Layout } from 'antd';
+import { Layout, Spin, Space } from 'antd';
 
 import 'antd/dist/antd.css';
 import './index.css';
+
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Home from './Home';
+
+
+//Lazy imports:
+const SponsorHome = React.lazy(() => import('./sponsors/pages/sponsorHome'));
+const SponsorRequest = React.lazy(() => import('./sponsors/pages/SponsorRequest'));
+const AdminDashboard = React.lazy(() => import('./admin/pages/AdminDashboard'));
+const SponsorshipRequestDetails = React.lazy(() => import('./admin/components/SponsorshipRequestDetails'));
 
 const { Content, Header } = Layout;
 
@@ -23,24 +25,30 @@ const App = () => {
               </Header>
               
               <Content className = "site-layout-content">
-                <Switch>
-                  <Route path="/" exact>
-                    <Home />
-                  </Route>
-                  <Route path="/sponsorHome/" exact>
-                    <SponsorHome />
-                  </Route>
-                  <Route path="/sponsorRequest/" exact>
-                    <SponsorRequest />
-                  </Route>
-                  <Route path="/:sid/sponsorshipRequestDetails/" exact>
-                    <SponsorshipRequestDetails />
-                  </Route>
-                  <Route path="/adminDashboard/" exact>
-                    <AdminDashboard />
-                  </Route>
-                  <Redirect to="/" />
-                </Switch>
+                <Suspense fallback={
+                  <Space size="middle">
+                    <Spin size="large" />
+                  </Space>
+                }>
+                  <Switch>
+                    <Route path="/" exact>
+                      <Home />
+                    </Route>
+                    <Route path="/sponsorHome/" exact>
+                      <SponsorHome />
+                    </Route>
+                    <Route path="/sponsorRequest/" exact>
+                      <SponsorRequest />
+                    </Route>
+                    <Route path="/:sid/sponsorshipRequestDetails/" exact>
+                      <SponsorshipRequestDetails />
+                    </Route>
+                    <Route path="/adminDashboard/" exact>
+                      <AdminDashboard />
+                    </Route>
+                    <Redirect to="/" />
+                  </Switch>
+                </Suspense>
               </Content>
             </Layout>
           </Router>;
